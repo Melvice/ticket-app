6a3bf538d86a7cbc7e0e2ef4edd48bafa61c3139
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2026-09-12 21:33:52 -0400

    crearing api for tickets (get, delete, post and patch)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import { ticketStore } from "./store";
+import { ticketsRouter } from "./routes/tickets";
 
 const app = express();
 
@@ -10,8 +12,14 @@ app.get("/health", (req, res) => {
     res.status(200).json({ status: "ok" });
 });
 
+app.use("/api/tickets", ticketsRouter);
+// Middleware pour gérer les routes non trouvées
+app.use((req, res) => {
+    res.status(404).json({ error: "Route non trouvée" });
+});
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
